5c1b05f78124edc5c3885e1b9c429de90a3fa6aa
mzahin01 <minhazahin@gmail.com>
2024-06-07 16:53:20 +0600

    registration and login completed

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,6 +1,85 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class FirestoreServices {
+  static saveUser(String name, email, uid) async {
+    await FirebaseFirestore.instance
+        .collection('users')
+        .doc(uid)
+        .set({'email': email, 'name': name});
+  }
+}
 
 class HomeController extends GetxController {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passController = TextEditingController();
+  TextEditingController nameController = TextEditingController();
+
+  TextEditingController logemailController = TextEditingController();
+  TextEditingController logpassController = TextEditingController();
+
   final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
+  Rx<User?> user = Rx(null);
+  RxnString username = RxnString();
+
+  @override
+  void onInit() {
+    FirebaseAuth.instance.authStateChanges().listen((User? u) {
+      user.value = u;
+      getUserName(u);
+    });
+    super.onInit();
+  }
+
+  Future<void> getUserName(User? user) async {
+    if (user == null) {
+      return;
+    }
+    final data = await FirebaseFirestore.instance
+        .collection('users')
+        .doc(user.uid)
+        .get();
+    username.value = data.data()?['name'];
+  }
+
+  void register() {
+    FirebaseAuth.instance
+        .createUserWithEmailAndPassword(
+      email: emailController.text,
+      password: passController.text,
+    )
+        .then(
+      (UserCredential cred) {
+        FirestoreServices.saveUser(
+          nameController.text,
+          emailController.text,
+          cred.user?.uid,
+        );
+        Get.back();
+      },
+    );
+  }
+
+  void login() async {
+    try {
+      await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: logemailController.text,
+        password: logpassController.text,
+      );
+      // Get.offAllNamed(Routes.HOME);
+      Get.snackbar('Contugralations!', 'Wellcome');
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        Get.snackbar('WEAK!', 'Password Provided is too weak');
+      } else if (e.code == 'email-already-in-use') {
+        Get.snackbar('REPEAT!', 'Email Provided already Exists');
+      } else if (e.code == 'invalid-credential') {
+        Get.snackbar('WRONG!', 'Email and Password does not match');
+      }
+    } catch (e) {
+      Get.snackbar('WEAK!', e.toString());
+    }
+  }
 }
--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,5 +1,7 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:mushaira/app/modules/home/modals_reg_log/modal.dart';
 import '../controllers/home_controller.dart';
 import '../widgets/widget_base.dart';
 
@@ -8,111 +10,157 @@ class HomeView extends GetView<HomeController> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      key: controller.scaffoldKey, // Assign GlobalKey to Scaffold
-      endDrawer: Drawer(
-        // Change drawer to endDrawer
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: <Widget>[
-            const DrawerHeader(
-              decoration: BoxDecoration(
-                color: Colors.blue,
+    return Obx(() {
+      return Scaffold(
+        key: controller.scaffoldKey,
+        endDrawer: const Drawer(
+          child: DrawerMade(),
+        ),
+        body: Container(
+          height: 75,
+          width: Get.width,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.2),
+                spreadRadius: 1,
+                blurRadius: 7,
               ),
-              child: Text(
-                'Drawer Header',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 24,
+            ],
+          ),
+          child: Row(
+            children: [
+              SizedBox(
+                width: Get.width / 17,
+              ),
+              SizedBox(
+                height: Get.width / 20,
+                width: Get.width / 10,
+                child: Image.asset("Assets/mulogo.png"),
+              ),
+              const SizedBox(
+                width: 15,
+              ),
+              const Module(
+                widgName: 'Home',
+                wayString: '/home',
+                iconman: Icons.home,
+              ),
+              const Module(
+                widgName: 'Courses',
+                wayString: '/home',
+                iconman: Icons.line_style,
+              ),
+              const Module(
+                widgName: 'Classroom',
+                wayString: '/home',
+                iconman: Icons.menu_book,
+              ),
+              Expanded(
+                child: SizedBox(
+                  width: Get.width / 25,
                 ),
               ),
-            ),
-            ListTile(
-              leading: const Icon(Icons.home),
-              title: const Text('Home'),
-              onTap: () {
-                Navigator.pop(context); // Close the drawer
-              },
-            ),
-            ListTile(
-              leading: const Icon(Icons.settings),
-              title: const Text('Settings'),
-              onTap: () {
-                Navigator.pop(context); // Close the drawer
-              },
-            ),
-            ListTile(
-              leading: const Icon(Icons.contacts),
-              title: const Text('Contacts'),
-              onTap: () {
-                Navigator.pop(context); // Close the drawer
-              },
-            ),
-          ],
-        ),
-      ),
-      body: Container(
-        height: 75,
-        width: Get.width,
-        decoration: BoxDecoration(
-          color: Colors.white,
-          boxShadow: [
-            BoxShadow(
-              color: Colors.black.withOpacity(0.2),
-              spreadRadius: 1,
-              blurRadius: 7,
-            ),
-          ],
-        ),
-        child: Row(
-          children: [
-            SizedBox(
-              width: Get.width / 17,
-            ),
-            SizedBox(
-              height: Get.width / 20,
-              width: Get.width / 10,
-              child: Image.asset("Assets/mulogo.png"),
-            ),
-            const SizedBox(
-              width: 15,
-            ),
-            const Module(
-              widgName: 'Home',
-              wayString: '/home',
-              iconman: Icons.home,
-            ),
-            const Module(
-              widgName: 'Courses',
-              wayString: '/home',
-              iconman: Icons.line_style,
-            ),
-            const Module(
-              widgName: 'Classroom',
-              wayString: '/home',
-              iconman: Icons.menu_book,
-            ),
-            Expanded(
-              child: SizedBox(
-                width: Get.width / 25,
+              if (controller.user.value == null) ...[
+                ElevatedButton(
+                  onPressed: () {
+                    Get.dialog(
+                      LoginModal(
+                        controller: controller,
+                      ),
+                      barrierDismissible: false,
+                    );
+                  },
+                  child: const Text('Log in'),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    Get.dialog(
+                      RegistrationModal(
+                        controller: controller,
+                      ),
+                      barrierDismissible: false,
+                    );
+                  },
+                  child: const Text('Register Now'),
+                ),
+              ] else
+                GestureDetector(
+                  onTap: () {
+                    controller.scaffoldKey.currentState?.openEndDrawer();
+                  },
+                  child: const Icon(
+                    Icons.cabin,
+                    size: 35,
+                  ),
+                ),
+              SizedBox(
+                width: Get.width / 12.6,
               ),
-            ),
-            GestureDetector(
-              onTap: () {
-                // Correctly open endDrawer on tap
-                controller.scaffoldKey.currentState?.openEndDrawer();
-              },
-              child: const Icon(
-                Icons.cabin,
-                size: 35,
-              ),
-            ),
-            SizedBox(
-              width: Get.width / 12.6,
-            ),
-          ],
+            ],
+          ),
         ),
-      ),
+      );
+    });
+  }
+}
+
+class DrawerMade extends StatelessWidget {
+  const DrawerMade({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      padding: EdgeInsets.zero,
+      children: <Widget>[
+        const DrawerHeader(
+          decoration: BoxDecoration(
+            color: Colors.blue,
+          ),
+          child: Text(
+            'Drawer Header',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 24,
+            ),
+          ),
+        ),
+        ListTile(
+          leading: const Icon(Icons.home),
+          title: const Text('Home'),
+          onTap: () {
+            Navigator.pop(context);
+          },
+        ),
+        ListTile(
+          leading: const Icon(Icons.settings),
+          title: const Text('Settings'),
+          onTap: () {
+            Navigator.pop(context);
+          },
+        ),
+        ListTile(
+          leading: const Icon(Icons.contacts),
+          title: const Text('Contacts'),
+          onTap: () {
+            Navigator.pop(context);
+          },
+        ),
+        const Expanded(
+            child: SizedBox(
+          height: 10,
+        )),
+        ElevatedButton(
+          onPressed: () {
+            FirebaseAuth.instance.signOut();
+            Navigator.pop(context);
+          },
+          child: const Icon(Icons.logout),
+        )
+      ],
     );
   }
 }
